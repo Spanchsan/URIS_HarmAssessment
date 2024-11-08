f462d9a1843f48d539db13d635220c5a90391594
Spanchsan <sanzharab@gmail.com>
2024-11-08 19:52:53 +0800

    Correction model

--- model/GraphTempMamba.py
+++ model/GraphTempMamba.py
@@ -427,6 +427,7 @@ class GraphModel(nn.Module):
         self.layer = layer
         if dim_in - pe_dim < 0:
             self.pe_dim = 0
+            print(self.pe_dim)
         self.node_emb = nn.Embedding(dim_in, dim_out - pe_dim)
         self.pe_lin = nn.Linear(dim_in - pe_dim, pe_dim)
         self.pe_norm = nn.BatchNorm1d(dim_in - pe_dim)
